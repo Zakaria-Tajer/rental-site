5a5cff26a525f502185ec49b173a0db0fca98651
Zakaria-Tajer <za.tajer@gmail.com>
2021-12-31 20:44:22 +0100

    Adding filter to reservation page

--- Reservation/index.js
+++ Reservation/index.js
@@ -4,6 +4,16 @@ const close_menu = document.getElementById('close')
 
 const chevron = document.getElementById('chevron')
 const input_field = document.getElementById('input-field')
+const shrink = document.getElementById('shrink')
+
+const empty = document.getElementById('empty')
+const chevron_empty = document.getElementById('chevron-empty')
+
+const select = document.getElementById('select')
+const chevron_select = document.getElementById('chevron-select')
+
+
+
 
 const hide = document.getElementById('hide')
 const chevron_right = document.getElementById('chevron-right')
@@ -23,7 +33,11 @@ close_menu.addEventListener('click', ()=> {
 
 chevron.addEventListener('click', ()=> {
     input_field.classList.toggle('active')
+    chevron.classList.toggle('active')
+    shrink.classList.toggle('active')
 })
+
+
 chevron_right.addEventListener('click', ()=> {
     hide.classList.toggle('active')
     chevron_right.classList.toggle('active')
@@ -34,3 +48,13 @@ rem.addEventListener('click', ()=>{
     window.location.assign('./payment/index.html')
 })
 
+
+chevron_empty.addEventListener('click', ()=>{
+    empty.classList.toggle('active')
+    chevron_empty.classList.toggle('active')
+
+})
+chevron_select.addEventListener('click', ()=> {
+    select.classList.toggle('active')
+    chevron_select.classList.toggle('active')
+})
--- Reservation/payment/app.js
+++ Reservation/payment/app.js
@@ -1,5 +1,6 @@
 const circle = document.getElementById('circle')
 const circle_drop = document.getElementById('drop-off-circle')
+const chevron_left = document.getElementById('chevron-left')
 
 circle.addEventListener('click', ()=>{
     circle.classList.toggle('active')
@@ -7,4 +8,7 @@ circle.addEventListener('click', ()=>{
 
 circle_drop.addEventListener('click', ()=>{
     circle_drop.classList.toggle('active')
+})
+chevron_left.addEventListener('click',()=> {
+    location.assign('../index.html')
 })
